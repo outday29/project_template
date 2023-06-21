6e1724663e4445a10f231a88233d079b19f61971
Eng Kheng <ongengkheng929@gmail.com>
2023-06-21 17:50:30 +0800

    update

---
+++ utils.py
@@ -0,0 +1,14 @@
+import time
+from contextlib import contextmanager
+from typing import Optional
+
+
+@contextmanager
+def timer(message: Optional[str] = None):
+    if message:
+        print(message, end=": ")
+    start_time = time.perf_counter()
+    yield
+    end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
+    print(f"{elapsed_time} seconds")
